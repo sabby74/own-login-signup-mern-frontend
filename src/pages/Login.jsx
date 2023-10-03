cc103a8e3b20ed5c4eaad87a9332f500c5fa87c2
dinesh nagpal <133826705+sabby74@users.noreply.github.com>
2023-10-03 17:22:20 -0400

    app wrkng

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -21,21 +21,7 @@ function Login() {
       console.error('Error during login:', error);
     }
   };
-  // const handleLogin = (e) => {
-  //   e.preventDefault();
-  //   axios
-  //     .post("https://own-loginsignup-mern.onrender.com/login", {
-  //       email,
-  //       password,
-  //     })
-  //     .then((result) => {
-  //       console.log(result);
-  //       if (result.data === "success") {
-  //         navigate("/home");
-  //       }
-  //     })
-  //     .catch((err) => console.log(err));
-  // };
+
 
   return (
 
@@ -48,41 +34,7 @@ function Login() {
     <button onClick={handleLogin}>Login</button>
     <Link to="/signup">Signup</Link>
   </div>
-    // <div className="login">
-    //   <div className="bg-yellow-200">
-    //     <h2>Login</h2>
-    //     <form onSubmit={handleLogin}>
-    //       <div className="bg-green-200">
-    //         <label htmlFor="email">
-    //           <strong>Email</strong>
-    //         </label>
-    //         <input
-    //           type="email"
-    //           placeholder="Enter Email"
-    //           name="email"
-    //           onChange={(e) => setEmail(e.target.value)}
-    //         />
-    //       </div>
-    //       <div className="bg-red-300 h-12">
-    //         <label htmlFor="password">
-    //           <strong>Password</strong>
-    //         </label>
-    //         <input
-    //           type="password"
-    //           placeholder="Enter Password"
-    //           name="password"
-    //           onChange={(e) => setPassword(e.target.value)}
-    //         />
-    //       </div>
-    //       <button type="submit" className="">
-    //         Login
-    //       </button>
-    //     </form>
-    //     <br />
-    //     <p>Create an Account here </p>
-    //     <Link to="/signup">Signup</Link>
-    //   </div>
-    // </div>
+
   );
 }
 
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -18,21 +18,13 @@ function Signup() {
       });
       console.log("Signup Response:", response.data);
       {
-        navigate("/home");
+        navigate("/login");
       }
     } catch (error) {
       console.error("Error during signup:", error);
     }
   };
-  //   const handleSignup = (e) => {
-  //     e.preventDefault()
-  //     axios.post("https://own-loginsignup-mern.onrender.com/signup", {name , email , password})
-  // .then (result => {console.log(result)
-  //     navigate('/login')
-
-  // })
-  //     .catch(err => console.log(err))
-  //   }
+ 
   return (
     <div className="signup">
       <h2>Signup</h2>
@@ -62,62 +54,7 @@ function Signup() {
         Login
       </Link>
     </div>
-    // <div className="signup">
-    //   <div className="">
-    //     <h2>Signup</h2>
-    //     <form onSubmit={handleSignup}>
-    //       <div className="bg-red-400 flex">
-    //         <label htmlFor="name">
-    //           <strong>Name</strong>
-    //         </label>
-    //         <input
-    //           type="text"
-    //           placeholder="Enter Name"
-    //           autoComplete="off"
-    //           name="name"
-    //           className=""
-    //           onChange={(e) => setName(e.target.value)}
-    //         />
-    //       </div>
-    //       <div className="mb-3">
-    //         <label htmlFor="email">
-    //           <strong>Email</strong>
-    //         </label>
-    //         <input
-    //           type="text"
-    //           placeholder="Enter Email"
-    //           autoComplete="off"
-    //           name="email"
-    //           className=""
-    //           onChange={(e) => setEmail(e.target.value)}
-    //         />
-    //       </div>
-    //       <div className="mb-3">
-    //         <label htmlFor="password">
-    //           <strong>Password</strong>
-    //         </label>
-    //         <input
-    //           type="password"
-    //           placeholder="Enter Password"
-    //           autoComplete="off"
-    //           name="password"
-    //           className=""
-    //           onChange={(e) => setPassword(e.target.value)}
-    //         />
-    //       </div>
-    //       <button type="submit" className="">
-    //         Register
-    //       </button>
-    //     </form>
-    //     <p>Already Have an Account!</p>
-    //     <Link
-    //       to="/login"
-    //       className=""
-    //     >
-    //       Login
-    //     </Link>
-    //   </div>
-    // </div>
+
   );
 }
 
