ae15ffedd0e825d728ab5b34c20e13472835b6a9
dinesh nagpal <133826705+sabby74@users.noreply.github.com>
2023-10-03 04:09:50 -0400

    trying different things

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,58 +8,75 @@ function Login() {
   const [password, setPassword] = useState();
   const navigate = useNavigate();
 
-  const handleLogin = (e) => {
-    e.preventDefault();
-    axios
-      .post("https://own-loginsignup-mern.onrender.com/login", {
-        email,
-        password,
-      })
-      .then((result) => {
-        console.log(result);
-        if (result.data === "success") {
-          navigate("/home");
-        }
-      })
-      .catch((err) => console.log(err));
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('http://your-backend-api/login', { email, password });
+      console.log('Login Response:', response.data);
+    } catch (error) {
+      console.error('Error during login:', error);
+    }
   };
+  // const handleLogin = (e) => {
+  //   e.preventDefault();
+  //   axios
+  //     .post("https://own-loginsignup-mern.onrender.com/login", {
+  //       email,
+  //       password,
+  //     })
+  //     .then((result) => {
+  //       console.log(result);
+  //       if (result.data === "success") {
+  //         navigate("/home");
+  //       }
+  //     })
+  //     .catch((err) => console.log(err));
+  // };
 
   return (
-    <div className="login">
-      <div className="bg-yellow-200">
-        <h2>Login</h2>
-        <form onSubmit={handleLogin}>
-          <div className="bg-green-200">
-            <label htmlFor="email">
-              <strong>Email</strong>
-            </label>
-            <input
-              type="email"
-              placeholder="Enter Email"
-              name="email"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div className="bg-red-300 h-12">
-            <label htmlFor="password">
-              <strong>Password</strong>
-            </label>
-            <input
-              type="password"
-              placeholder="Enter Password"
-              name="password"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <button type="submit" className="">
-            Login
-          </button>
-        </form>
-        <br />
-        <p>Create an Account here </p>
-        <Link to="/signup">Signup</Link>
-      </div>
-    </div>
+
+    <div>
+    <h2>Login</h2>
+    <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+    <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+    <button onClick={handleLogin}>Login</button>
+    <Link to="/signup">Signup</Link>
+  </div>
+    // <div className="login">
+    //   <div className="bg-yellow-200">
+    //     <h2>Login</h2>
+    //     <form onSubmit={handleLogin}>
+    //       <div className="bg-green-200">
+    //         <label htmlFor="email">
+    //           <strong>Email</strong>
+    //         </label>
+    //         <input
+    //           type="email"
+    //           placeholder="Enter Email"
+    //           name="email"
+    //           onChange={(e) => setEmail(e.target.value)}
+    //         />
+    //       </div>
+    //       <div className="bg-red-300 h-12">
+    //         <label htmlFor="password">
+    //           <strong>Password</strong>
+    //         </label>
+    //         <input
+    //           type="password"
+    //           placeholder="Enter Password"
+    //           name="password"
+    //           onChange={(e) => setPassword(e.target.value)}
+    //         />
+    //       </div>
+    //       <button type="submit" className="">
+    //         Login
+    //       </button>
+    //     </form>
+    //     <br />
+    //     <p>Create an Account here </p>
+    //     <Link to="/signup">Signup</Link>
+    //   </div>
+    // </div>
   );
 }
 
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -9,72 +9,94 @@ function Signup() {
   const [password, setPassword] = useState("");
 const navigate = useNavigate()
 
-  const handleSignup = (e) => {
-    e.preventDefault()
-    axios.post("https://own-loginsignup-mern.onrender.com/signup", {name , email , password})
-.then (result => {console.log(result)
-    navigate('/login')
 
-})
-    .catch(err => console.log(err))
-  }
+const handleSignup = async () => {
+    try {
+      const response = await axios.post('https://own-loginsignup-mern.onrender.com/signup', { name , email, password });
+      console.log('Signup Response:', response.data);
+    } catch (error) {
+      console.error('Error during signup:', error);
+    }
+  };
+//   const handleSignup = (e) => {
+//     e.preventDefault()
+//     axios.post("https://own-loginsignup-mern.onrender.com/signup", {name , email , password})
+// .then (result => {console.log(result)
+//     navigate('/login')
+
+// })
+//     .catch(err => console.log(err))
+//   }
   return (
-    <div className="signup">
-      <div className="">
-        <h2>Signup</h2>
-        <form onSubmit={handleSignup}>
-          <div className="bg-red-400 flex">
-            <label htmlFor="name">
-              <strong>Name</strong>
-            </label>
-            <input
-              type="text"
-              placeholder="Enter Name"
-              autoComplete="off"
-              name="name"
-              className=""
-              onChange={(e) => setName(e.target.value)}
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="email">
-              <strong>Email</strong>
-            </label>
-            <input
-              type="text"
-              placeholder="Enter Email"
-              autoComplete="off"
-              name="email"
-              className=""
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="password">
-              <strong>Password</strong>
-            </label>
-            <input
-              type="password"
-              placeholder="Enter Password"
-              autoComplete="off"
-              name="password"
-              className=""
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <button type="submit" className="">
-            Register
-          </button>
-        </form>
-        <p>Already Have an Account!</p>
-        <Link
+    <div>
+    <h2>Signup</h2>
+    <input type="name" placeholder="name" value={name} onChange={(e) => setName(e.target.value)} />
+    <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+    <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+    <button onClick={handleSignup}>Signup</button>
+    <Link
           to="/login"
           className=""
         >
           Login
         </Link>
-      </div>
-    </div>
+  </div>
+    // <div className="signup">
+    //   <div className="">
+    //     <h2>Signup</h2>
+    //     <form onSubmit={handleSignup}>
+    //       <div className="bg-red-400 flex">
+    //         <label htmlFor="name">
+    //           <strong>Name</strong>
+    //         </label>
+    //         <input
+    //           type="text"
+    //           placeholder="Enter Name"
+    //           autoComplete="off"
+    //           name="name"
+    //           className=""
+    //           onChange={(e) => setName(e.target.value)}
+    //         />
+    //       </div>
+    //       <div className="mb-3">
+    //         <label htmlFor="email">
+    //           <strong>Email</strong>
+    //         </label>
+    //         <input
+    //           type="text"
+    //           placeholder="Enter Email"
+    //           autoComplete="off"
+    //           name="email"
+    //           className=""
+    //           onChange={(e) => setEmail(e.target.value)}
+    //         />
+    //       </div>
+    //       <div className="mb-3">
+    //         <label htmlFor="password">
+    //           <strong>Password</strong>
+    //         </label>
+    //         <input
+    //           type="password"
+    //           placeholder="Enter Password"
+    //           autoComplete="off"
+    //           name="password"
+    //           className=""
+    //           onChange={(e) => setPassword(e.target.value)}
+    //         />
+    //       </div>
+    //       <button type="submit" className="">
+    //         Register
+    //       </button>
+    //     </form>
+    //     <p>Already Have an Account!</p>
+    //     <Link
+    //       to="/login"
+    //       className=""
+    //     >
+    //       Login
+    //     </Link>
+    //   </div>
+    // </div>
   );
 }
 
